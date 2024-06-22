8d1ee3c3b3e2638f02882d00ac018aaf90cdd210
marinus-g <marinusmail@protonmail.com>
2024-06-22 13:59:57 +0200

    MM-11 updated server.js to fix 404 file not found

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/<app-name>'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/<app-name>/browser/index.html'));});
+    '/dist/modules-frontend/browser/index.html'));});
 app.listen(process.env.PORT || 8080);
