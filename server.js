bbd8676694a8331f198c185e94f6dd278fb6b4e3
marinus-g <marinusmail@protonmail.com>
2024-06-22 14:10:21 +0200

    MM-11 edited server.js to allow gooogle fonts, and unsafe inline css

--- server.js
+++ server.js
@@ -1,8 +1,37 @@
 const express = require('express');
 const path = require('path');
+const helmet = require('helmet');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/modules-frontend/browser/index.html'));});
-app.listen(process.env.PORT || 8080);
+
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "http://localhost:8080"],
+      imgSrc: ["'self'", "data:", "http://localhost:8080"],
+      styleSrc: ["'self'", "http://localhost:8080", "https://fonts.googleapis.com", "'unsafe-inline'"],
+      connectSrc: ["'self'", "http://localhost:8080"],
+      fontSrc: ["'self'", "http://localhost:8080", "https://fonts.gstatic.com"],
+      objectSrc: ["'none'"],
+      upgradeInsecureRequests: [],
+    },
+  },
+}));
+
+app.use(express.static(path.join(__dirname, 'dist/modules-frontend/browser')));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'dist/modules-frontend/browser', 'index.html'));
+});
+
+app.use((req, res, next) => {
+  if (req.path.endsWith('.js')) {
+    res.setHeader('Content-Type', 'application/javascript');
+  }
+  next();
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server läuft auf Port ${port}`);
+});
