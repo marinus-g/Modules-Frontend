c69956232e7c3f98a020b1834eb4d4dbc3164d29
Bockkwurst <147827734+Bockkwurst@users.noreply.github.com>
2024-06-28 15:08:23 +0200

    component layout ready - table view not scrollable

---
+++ src/app/module-view/module-view.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+import {DataService} from "../Service/data.service";
+import {NgForOf} from "@angular/common";
+
+@Component({
+  selector: 'app-module-view',
+  standalone: true,
+  imports: [
+    MatIcon,
+    NgForOf
+  ],
+  templateUrl: './module-view.component.html',
+  styleUrl: './module-view.component.css'
+})
+export class ModuleViewComponent implements OnInit {
+  classesData: any;
+  tableData: any;
+
+  constructor(private dataService: DataService) {
+  }
+
+  ngOnInit() {
+    this.dataService.getData().subscribe(data =>{
+      this.classesData = data.classes;
+      this.tableData = data.table;
+    });
+  }
+}
