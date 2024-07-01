bf5c6bf2a03f74b996f9f0a9f7c833f4ab858ff1
marinus-g <marinusmail@protonmail.com>
2024-07-01 08:35:59 +0200

    MM-44 added oauth redirect route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from "./page/login/login.component";
+import {OAuth2CallbackComponent} from "./component/oauth2-callback/oauth2-callback.component";
 
 export const routes: Routes = [
   {
     path: 'login',
     title: 'Login',
     component: LoginComponent
+  },
+  {
+    path: 'oauth2/code/azure',
+    title: 'Microsoft Login',
+    component: OAuth2CallbackComponent
   }
 ];
